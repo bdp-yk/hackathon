c537e4e69ceeb9ae6967c815a3a3653016b3f09e
BigDaddyPanda <yassinkisrawi42@gmail.com>
2019-11-23 21:58:07 +0100

    init

---
+++ tripbook/src/layouts/MyLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="../assets/sad.svg" />
+          </q-avatar>
+          TripBook
+        </q-toolbar-title>
+        <q-btn flat round dense icon="group_add" class="q-mr-xs" />
+        <q-btn to="/trips" label="Start new Trip" color="positive" unelevated />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
---
+++ tripbook/src/pages/Index.vue
@@ -0,0 +1,70 @@
+<template>
+  <q-page class="row flex flex-center q-gutter-md q-pa-lg">
+    <q-card class="col-12 row justify-center q-pa-md q-gutter-y-md column">
+      <q-input
+        v-model="search"
+        outlined
+        placeholder="Search"
+      >
+        <template v-slot:append>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+    </q-card>
+    <q-card class="my-card" v-for="i in 5" :key="i">
+      <img src="https://cdn.quasar.dev/img/parallax2.jpg">
+
+      <q-list>
+        <q-item clickable>
+          <q-item-section avatar>
+            <q-icon color="primary" name="local_bar" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>Bar XYZ</q-item-label>
+            <q-item-label caption>Have a drink.</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable>
+          <q-item-section avatar>
+            <q-icon color="red" name="local_gas_station" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>Gas Station</q-item-label>
+            <q-item-label caption>Fill your gas tank.</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable>
+          <q-item-section avatar>
+            <q-icon color="amber" name="local_movies" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>Cinema XYZ</q-item-label>
+            <q-item-label caption>Watch a movie.</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      focusFilter: false
+    }
+  }
+}
+</script>
+
+<style>
+  .my-card{
+    max-width: 50vh;
+  }
+</style>
